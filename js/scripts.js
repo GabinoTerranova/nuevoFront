44da39cbeae0a10ecff2a15930022d3ce8b59fe0
GabinoTerranova <146039885+GabinoTerranova@users.noreply.github.com>
2024-07-08 21:22:05 -0300

    actualizaciones

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+/*!
+* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+function Reservations() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://assets.calendly.com/assets/external/widget.js';
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <section id="reservations">
+            <h2>Reservas</h2>
+            <div className="calendly-inline-widget" data-url="https://calendly.com/quick_tech/visit?hide_gdpr_banner=1" style={{ minWidth: '320px', height: '700px' }}></div>
+        </section>
+    );
+}
